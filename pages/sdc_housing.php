bfa94606057e8944004516c1aa7bd06cd1a436d4
mgplanas <mgplanas@gmail.com>
2019-09-30 11:45:08 -0300

    FIX Accessos

--- pages/sdc_housing.php
+++ pages/sdc_housing.php
@@ -28,7 +28,7 @@ if(isset($_GET['aksi']) == 'delete'){
 
 
 //Get user query
-$persona = mysqli_query($con, "SELECT * FROM persona WHERE email='$user'");
+$persona = mysqli_query($con, "SELECT * FROM persona WHERE email='$user'  AND borrado = 0");
 $rowp = mysqli_fetch_assoc($persona);
 $id_rowp = $rowp['id_persona'];
 
