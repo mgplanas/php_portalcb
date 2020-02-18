14817d101eb278f0df865dd7fe1cefd6d343d678
mgplanas <mgplanas@gmail.com>
2020-02-18 10:15:14 -0300

    Cambios visibilidad de campos de mejoras

--- pages/mejoras.php
+++ pages/mejoras.php
@@ -910,7 +910,7 @@ desired effect
             }],
       'columnDefs': [
       {
-          'targets': [ 9,10,11,12,13,14,15,16 ],
+          'targets': [ 10,11,12,13,14,15,16,17 ],
           'visible': false
       }
   ]
