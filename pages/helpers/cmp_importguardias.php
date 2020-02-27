38a660b3f59ac16dd664f9888cb42e16b538da87
mgplanas <mgplanas@gmail.com>
2020-02-27 14:54:38 -0300

    TEST

--- pages/helpers/cmp_importguardias.php
+++ pages/helpers/cmp_importguardias.php
@@ -2,20 +2,20 @@
     include("../../conexion.php");
     
     
-    $result = new stdClass();
-    try {
-        // Turn autocommit off
-        mysqli_autocommit($con,FALSE);
-        mysqli_begin_transaction($con);
-    } catch (Exception $e) {
-        $result->error1=$e->getMessage();
-    }
-    try {
-        mysqli_commit($con);
-        mysqli_autocommit($con,TRUE);
-    } catch (Exception $e) {
-        $result->error2=$e->getMessage();
-    }
+    // $result = new stdClass();
+    // try {
+    //     // Turn autocommit off
+    //     mysqli_autocommit($con,FALSE);
+    //     mysqli_begin_transaction($con);
+    // } catch (Exception $e) {
+    //     $result->error1=$e->getMessage();
+    // }
+    // try {
+    //     mysqli_commit($con);
+    //     mysqli_autocommit($con,TRUE);
+    // } catch (Exception $e) {
+    //     $result->error2=$e->getMessage();
+    // }
 
 
     $result->ok = false;
